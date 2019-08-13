49b8dd94f8582153d3f074c91ebd595ba1245c38
github username <github email address>
2019-08-13 10:32:40 -0700

    crushed it

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,37 @@
 class Customer
   attr_reader :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+end
+
+def add_review(restaurant, content, rating)
+    # Review.all << Review.new(self, restaurant, rating, content)
+end
+
+def num_reviews
+    Review.all.select {|rev| rev.customer == self }.length       
+end
+
+def restaurants 
+  list = Review.all.select do |rev| 
+    if rev.customer == self 
+        rev.restaurant
+    end
+  end
+  return list.uniq
+end
+
 end
---
+++ config/reviews.rb
@@ -0,0 +1,19 @@
+# class Reviews
+#     attr_reader :customer, :restaurant
+#     attr_writer
+#     attr_accessor :rating, :content
+
+#     @@all = []
+
+#     def initialize(customer, restaurant, rating, content)
+#         @customer = customer
+#         @restaurant = restaurant
+#         @rating = rating.to_i
+#         @content = content
+#         @@all << self
+#     end
+
+#     def self.all
+#         @@all
+#     end
+# end
