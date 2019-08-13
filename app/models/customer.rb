e89924c29e51132e224b7482188ca6c7a6d870e2
github scarlettutopia <github scarlettutopia@aol.com>
2019-08-13 10:36:43 -0700

    failed

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,33 @@
 class Customer
   attr_reader :first_name, :last_name
+  
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self 
   end
+
+  def self.all
+    @@all
+  end
+
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  #def add_review(restaurant, content, rating)
+
+  #end
+
+  def num_reviews
+    Review.all.select {|n| n.name}
+  end
+
+  #def restaurants
+    Self.review.all.select 
+  #end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,25 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+
+
+joe = Customer.new("Joe Dirt")
+sarah = Customer.new("Sarah Flow")
+ben = Customer.new("Ben Lee")
+hannah = Customer.new("Hannah Mae")
+
+indochine = Restaurant.new("Indochine")
+umis = Restaurant.new("Umi's")
+harmon = Restaurant.new("Harmon")
+table47 = Restaurant.new("Table 47")
+
+r1 = Review.new("Joe Dirt", "Umi's", 4)
+r2 = Review.new("Sarah Flow", "Table 47", 5)
+r3 = Review.new ("Ben Lee", "Indochine", 4)
+r4 = Review.new("Hannah Mae", "Harmon", 3)
+r5 = Review.new("Hannah Mae", "Table 47", 3)
+r6 = Review.new("Sarah Flow", "Harmon", 2)
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
