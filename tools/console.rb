06f08a06f1a131084c45ba000a764db17276ea24
github username <github email address>
2019-08-13 10:29:57 -0700

    DID I PASS

--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,13 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+c = Customer.new("h","h")
+cr = Customer.new("fn","ln")
+r = Restaurant.new("name")
+
+h = c.add_review(r, "it twas meh", 3)
+l = c.add_review(r, "it tws meh", 1)
+r = cr.add_review(r, "it tw", 3)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
