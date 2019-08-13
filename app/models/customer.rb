65370c6d87b1189dbb52563ca1e98190aef3328d
github username <github email address>
2019-08-13 11:55:27 -0700

    yay

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,68 @@
 class Customer
   attr_reader :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def add_review(restaurant, rating, content) # switched rating and content order for consistency
+    Review.new(self, restaurant, rating, content)
+  end
+
+  def my_reviews # Helper method for #num_reviews and #restaurants
+
+    # I know it's ugly!
+
+    
+    my_reviews_array = []
+    Review.all.each do |review| # IF TIME: choose a better enumerator
+      if review.customer == self
+        my_reviews_array << review
+      end
+    end
+    my_reviews_array
+  end
+
+  def num_reviews
+    self.my_reviews.count
+  end
+
+  def restaurants
+    my_restaurants = self.my_reviews.map do |review|
+      review.restaurant
+    end
+    my_restaurants.map do |my_rest|
+      my_rest.name
+    end
+  end
 end
+
+
+
+
+
+### DELIVERABLES ###
+
+### COMPLETED ###
+
+# Customer#add_review(restaurant, content, rating)
+    #   given a restaurant object, some review content (as a string), and a star rating (as an integer), creates a new review and associates it with that customer and restaurant.
+
+# Customer#num_reviews
+    #   Returns the total number of reviews that a customer has authored
+
+# Customer#restaurants
+    #   Returns a unique array of all restaurants a customer has reviewed
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,43 @@
 class Review
-  
+    attr_reader :customer, :restaurant
+    attr_accessor :rating, :content
+        # Deliverables noted specifically that customer and restaurant should NOT change, but didn't say so for rating or content.
+
+    @@all = []
+
+    def initialize(customer, restaurant, rating, content)
+        # IF TIME:
+            # RATING MUST BE INTEGER
+            # CONTENT MUST BE STRING
+        @customer = customer
+        @restaurant = restaurant
+        @rating = rating
+        @content = content
+
+        @@all << self
+    end
+
+  def self.all
+    @@all
+  end
+
+
 end
 
+### DELIVERABLES ###
+
+### COMPLETED ###
+
+# Review#customer
+    # returns the customer object for that given review
+    # Once a review is created, I should not be able to change the author
+
+# Review#restaurant
+    # returns the restaurant object for that given review
+    # Once a review is created, I should not be able to change the restaurant
+
+# Review#rating
+    # returns the star rating for a restaurant. This should be an integer from 1-5
+
+# Review#content
+    # returns the review content, as a string, for a particular review
